a1a746746ca030636023e8e3dd864ce83e831f81
Cynthia <cynlioness@gmail.com>
2014-04-06 17:35:38 -0400

    Separated mother text

--- colloquy/Mother.rb
+++ colloquy/Mother.rb
@@ -1,18 +1,20 @@
 module Mother
-	askdidsee = "Mom, do you know anything about my invention having been stolen? Did you see anything, anything at all?"
-	didsee = "Well, When I was unpacking this morning your neighbor did come by. He said that he'd like to say he's forgiven you for...what was it...running over his dog? Ah, yes, that was it."
+	askdidsee1 = "Mom, do you know anything about my invention having been stolen?"
+	askdidsee2 = "Did you see anything, anything at all?"
+	didsee = "Well, When I was unpacking this morning\nyour neighbor did come by.\nHe said that he'd like to say he's forgiven you for...\nwhat was it...\nrunning over his dog?\nAh, yes, that was it."
 	werehehere = "Well...if he was here on the day of the crime, I better go question him."
 	Says = Hash[
 		"" => "Hello son",
 		"Did you do it?" => "You should be ashamed of yourself!",
 		"Make yourself at home" => "Of course sweetie, I always do!",
-		askdidsee => didsee,
+		askdidsee1 => didsee,
+		askdidsee2 => didsee,
 		werehehere => "OK sweetie, have fun!",
 		"goodbye" => 0]
 	Hears = Hash[
-		"Hello son" => ["Did you do it?", "Make yourself at home", askdidsee, "goodbye"],
-		"Of course sweetie, I always do!" => ["Did you do it?", askdidsee, "goodbye"],
-		"You should be ashamed of yourself!" => ["Make yourself at home", askdidsee, "goodbye"],
+		"Hello son" => ["Did you do it?", "Make yourself at home", askdidsee1, askdidsee2, "goodbye"],
+		"Of course sweetie, I always do!" => ["Did you do it?", askdidsee1, askdidsee2, "goodbye"],
+		"You should be ashamed of yourself!" => ["Make yourself at home", askdidsee1, askdidsee2, "goodbye"],
 		"OK sweetie, have fun!" => ["Make yourself at home", "goodbye"],
 		didsee => [werehehere, "goodbye"]]
 end
