505ef4edcca20ee6e99dcf42940a0d76c261add8
cynlioness <cynlioness@users.noreply.github.com>
2014-04-08 19:55:03 -0400

    Update Eli.rb

--- colloquy/Eli.rb
+++ colloquy/Eli.rb
@@ -1,17 +1,19 @@
 module Eli
-	ungreeting = "I am your neighbor. You're not a pleasant person."
-	inquiry = "Have you an idea of the location of my invention?"
-	insult = "No, of course not. Your invention couldn't possibly have value to anyone."
-	departure1 = "Well then. It was made of expensive circuitry."
+	ungreeting = "Don't you have the decency not to come around at such an early hour?! What do you want?"
+	inquiry = "Do you know anything about my missing invention?"
+	insult = "No, of course not. I'm surprised someone bothered stealing it. /nYour invention couldn't possibly have value to anyone."
+	departure1 = "Do you have an alabi for last night?"
+	incrimination = "No. Look, I didn't sleep perticularly well last night and I'd like you to just go away now."
 	departure2 = "I wonder if you do have it. Goodbye."
 	Says = Hash[
 		"" => ungreeting,
 		inquiry => insult,
-		departure1 => 0,
+		departure1 => incrimination,
 		departure2 => 0
 	]
 	Hears = Hash[
 		ungreeting => [inquiry],
 		insult => [departure1, departure2]
+		imcrimination => [departure2]
 	]
 end
