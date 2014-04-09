ac50ca63159acecf9dca598110704f7969f29775
cynlioness <cynlioness@users.noreply.github.com>
2014-04-08 21:00:50 -0400

    Update Hospital.rb

--- colloquy/Hospital.rb
+++ colloquy/Hospital.rb
@@ -2,8 +2,11 @@ module Hospital
 	Says = Hash[
 		"" => "Hello, how may I help you?",
 		"Hello. I would like to ask weather /na Mr. Graham Green was working last night?" => "Yes sir, he was. He had the night shift"
-		"goodbye" => 0]
+		"All night? No breaks?" => "Yes sir, he was here the entire night."
+		"Thank you very much. Goodbye." => 0]
 	Hears = Hash[
 		"Hello, how may I help you?" => ["Hello. I would like to ask weather /na Mr. Graham Green was working last night?","goodbye"]
+		"Yes sir, he was. He had the night shift" => "All night? No breaks?"
+		"Yes sir, he was here the entire night." => "Thank you very much. Goodbye."
 	]
 end
